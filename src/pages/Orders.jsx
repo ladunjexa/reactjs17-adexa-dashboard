637c6ce11ebef10445efab51337e288c561e18bf
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-03-21 03:42:34 +0200

    Update Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -18,7 +18,7 @@ import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
 import { Header } from "../components";
 const Orders = () => {
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
       <GridComponent
         id="gridcomp"
